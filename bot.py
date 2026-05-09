eb4302a49afdc850b938644271647717025722e0
Aesteh <azkaarya2303@gmail.com>
2026-05-09 11:29:00 +0700

    Refactor bot to use commands extension and add features

--- bot.py
+++ bot.py
@@ -1,36 +1,49 @@
-from email import message
-
 import discord
-import bot_logic
-from bot_logic import gen_pass 
-from bot_logic import gen_emodji 
-from bot_logic import flip_coin
-# Variabel intents menyimpan hak istimewa bot
+from discord.ext import commands
+from settings import setting
+
 intents = discord.Intents.default()
-# Mengaktifkan hak istimewa message-reading
 intents.message_content = True
-# Membuat bot di variabel klien dan mentransfernya hak istimewa
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Kita telah masuk sebagai {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(description="Generates a random password")
+async def gen_pass(ctx, pass_length: int):
+    from bot_logic import gen_pass
+    password = gen_pass(pass_length)
+    await ctx.send(f'Your generated password is: {password}')
+
+@bot.command(description="Flips a coin")
+async def flip_coin(ctx):
+    from bot_logic import flip_coin
+    result = flip_coin()
+    await ctx.send(f'The coin landed on: {result}')
+
+@bot.command(description="Rolls a die with a specified number of sides")
+async def roll_die(ctx, sides: int):
+    from bot_logic import roll_die
+    result = roll_die(sides)
+    await ctx.send(f'You rolled a {result} on a {sides}-sided die.')
+
+@bot.command(description="Picks a random item from a list")
+async def pick_random(ctx, *items):
+    from bot_logic import pick_random
+    if not items:
+        await ctx.send('Please provide a list of items to choose from.')
         return
-    if message.content.startswith('$halo'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$check'):
-        await message.channel.send("test")
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$emoji'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$pass'):
-        await message.channel.send("Password: " + gen_pass(10))
-        
-client.run("")
+    result = pick_random(items)
+    await ctx.send(f'I picked: {result}')
+
+bot.run(setting["TOKEN"])
