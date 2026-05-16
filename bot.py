a1347821f3d05ef3d9f6c333b2a5df4407824fe9
Aesteh <azkaarya2303@gmail.com>
2026-05-16 09:57:00 +0700

    Implement guessing game command
    Added a guessing game command that prompts the user to guess a number between 1 and 10, with feedback on their guess.

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import asyncio
 from discord.ext import commands
 from settings import setting
 
@@ -45,5 +47,24 @@ async def pick_random(ctx, *items):
         return
     result = pick_random(items)
     await ctx.send(f'I picked: {result}')
+
+@bot.command(name='guess')
+async def guess(ctx):
+    await ctx.send('Guess a number between 1 and 10.')
+
+    def is_correct(m):
+        return m.author == ctx.author and m.content.isdigit() and m.channel == ctx.channel
+
+    answer = random.randint(1, 10)
+
+    try:
+        guess_msg = await bot.wait_for('message', check=is_correct, timeout=5.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'Sorry, you took too long it was {answer}.')
+
+    if int(guess_msg.content) == answer:
+        await ctx.send('You are right!')
+    else:
+        await ctx.send(f'Oops. It is actually {answer}.')
 
 bot.run(setting["TOKEN"])
