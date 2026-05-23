dee5b276417b2e2fcf357a660152b626cd75af38
Aesteh <azkaarya2303@gmail.com>
2026-05-23 09:57:43 +0700

    Refactor bot commands and add new features
    Refactor bot commands for clarity and consistency. Added new commands for memes, ducks, anime, and cats.

--- bot.py
+++ bot.py
@@ -1,51 +1,51 @@
-import discord
+import os
 import random
 import asyncio
+import discord
+import requests
 from discord.ext import commands
 from settings import setting
+from bot_logic import gen_pass, flip_coin, roll_die, pick_random, get_anime_image_url, get_duck_image, get_cat_image
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
+# --- Commands ---
+
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Hi! I am a bot {bot.user}!')
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
+async def heh(ctx, count_heh: int = 5):
     await ctx.send("he" * count_heh)
 
 @bot.command(description="Generates a random password")
-async def gen_pass(ctx, pass_length: int):
-    from bot_logic import gen_pass
-    password = gen_pass(pass_length)
-    await ctx.send(f'Your generated password is: {password}')
+async def password(ctx, pass_length: int):
+    result = gen_pass(pass_length)
+    await ctx.send(f'Your generated password is: {result}')
 
 @bot.command(description="Flips a coin")
-async def flip_coin(ctx):
-    from bot_logic import flip_coin
+async def flip(ctx):
     result = flip_coin()
     await ctx.send(f'The coin landed on: {result}')
 
 @bot.command(description="Rolls a die with a specified number of sides")
-async def roll_die(ctx, sides: int):
-    from bot_logic import roll_die
+async def roll(ctx, sides: int):
     result = roll_die(sides)
     await ctx.send(f'You rolled a {result} on a {sides}-sided die.')
 
 @bot.command(description="Picks a random item from a list")
-async def pick_random(ctx, *items):
-    from bot_logic import pick_random
+async def pick(ctx, *items):
     if not items:
         await ctx.send('Please provide a list of items to choose from.')
         return
-    result = pick_random(items)
+    result = pick_random(items) 
     await ctx.send(f'I picked: {result}')
 
 @bot.command(name='guess')
@@ -67,4 +67,33 @@ async def guess(ctx):
     else:
         await ctx.send(f'Oops. It is actually {answer}.')
 
+@bot.command(name='meme')
+async def mem(ctx):
+    all_local_images = os.listdir('images')
+    img_name = random.choice(all_local_images)
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+
+@bot.command(name='duck')
+async def duck(ctx):
+    duck_url = get_duck_image()
+    if duck_url:
+        await ctx.send(duck_url)
+
+@bot.command(name='anime')
+async def anime(ctx, *, keyword: str):
+    anime_url = get_anime_image_url(keyword)
+    if anime_url:
+        await ctx.send(anime_url)
+    else:
+        await ctx.send('No anime found with that keyword.')
+                       
+@bot.command(name='cat')
+async def cat(ctx):
+    cat_url = get_cat_image()
+    if cat_url:             
+        await ctx.send(cat_url)
+    else:        await ctx.send('Could not fetch a cat image at the moment.')
+                       
 bot.run(setting["TOKEN"])
