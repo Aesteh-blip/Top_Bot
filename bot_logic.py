680a98782c7ef6d3104af5b379c75388e1d07379
Aesteh <azkaarya2303@gmail.com>
2026-05-23 09:58:08 +0700

    Enhance bot with image fetching functions
    Added functions to fetch random images of anime, ducks, and cats.

--- bot_logic.py
+++ bot_logic.py
@@ -1,7 +1,8 @@
 import random
+import requests
 
 def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
+    elements = "+-/*!&$#?=@<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     password = ""
 
     for i in range(pass_length):
@@ -9,14 +10,42 @@ def gen_pass(pass_length):
 
     return password
 
+
 def flip_coin():
     coins = ["Heads", "Tails"]
     return random.choice(coins)
 
+
 def roll_die(sides):
-    sides = [4, 6, 8, 10, 12, 20]
+    if sides <= 0:
+        sides = 6
     return random.randint(1, sides)
 
+
 def pick_random(items):
-    items = ["Bagel", "Croissant", "Muffin", "Donut", "Scone"]
     return random.choice(items)
+
+def get_anime_image_url(keyword):
+    url = f'https://api.jikan.moe/v4/anime?q={keyword}&limit=1'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data['data']:
+            return data['data'][0]['images']['jpg']['image_url']
+    return None
+
+def get_duck_image():
+    response = requests.get('https://random-d.uk/api/v2/random')
+    if response.status_code == 200:
+        data = response.json()
+        return data['url']
+    else:
+        return None
+    
+def get_cat_image():
+    response = requests.get('https://api.thecatapi.com/v1/images/search')
+    if response.status_code == 200:
+        data = response.json()
+        return data[0]['url']
+    else:
+        return None
